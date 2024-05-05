9d1001dba84a5adc7bef34417e195d3f4599f2e4
Goodeny <32174827+goodeny@users.noreply.github.com>
2024-05-05 03:34:49 -0300

    Update Ordenador.sql

--- Ordenador.sql
+++ Ordenador.sql
@@ -2,6 +2,13 @@ create database Ordenador;
 use Ordenador;
 create table Data(
 	id int auto_increment primary key,
+    size int,
+	time varchar(100)
+);
+
+create table Data2(
+	id int auto_increment primary key,
+    name varchar(100),
 	size int,
 	time varchar(100)
 );
