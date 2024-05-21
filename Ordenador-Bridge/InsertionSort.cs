8f86521c996fa52cc3faad3cfded7e8e6b1b6497
Goodeny <32174827+goodeny@users.noreply.github.com>
2024-05-21 19:20:29 -0300

    Add files via upload

---
+++ Ordenador-Bridge/InsertionSort.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordenador
+{
+    internal class InsertionSort : ISortImplementation
+    {
+        public void Sort(int[] array)
+        {
+            foreach (int i in SortArray(array, array.Length))
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+        public int[] SortArray(int[] array, int length)
+        {
+            for (int i = 1; i < length; i++)
+            {
+                var key = array[i];
+                var flag = 0;
+                for (int j = i - 1; j >= 0 && flag != 1;)
+                {
+                    if (key < array[j])
+                    {
+                        array[j + 1] = array[j];
+                        j--;
+                        array[j + 1] = key;
+                    }
+                    else flag = 1;
+                }
+            }
+            return array;
+        }
+    }
+}
---
+++ Ordenador-Bridge/Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordenador
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Repository repository = new Repository();
+            Stopwatch stopWatch = new Stopwatch();   
+            Program program = new Program();
+            GenerateArray generateArray = new GenerateArray();
+
+            int[] sizes = { 256, 512, 1024, 2048, 5096 };
+            
+            foreach (int sizex in sizes)
+            {
+                stopWatch.Start();
+                int[] array = generateArray.CreateArray(sizex);
+
+                var context = new Context(new SortClient(new CountingSort()));
+                context.ExecuteStrategy(array);
+
+                var contextClone = (Context)context.Clone();
+                contextClone.SetSortAbstraction(new SortClient(new InsertionSort()));
+                contextClone.ExecuteStrategy(array);
+
+                var contextClone2 = (Context)context.Clone();
+                contextClone2.SetSortAbstraction(new SortClient(new QuickSort()));
+                contextClone2.ExecuteStrategy(array);
+
+                var contextClone3 = (Context)context.Clone();
+                contextClone3.SetSortAbstraction(new SortClient(new MargeSort()));
+                contextClone3.ExecuteStrategy(array);
+
+                // Adicione aqui outras estratégias de ordenação, se necessário
+
+                stopWatch.Stop();
+                TimeSpan tsTotal = stopWatch.Elapsed;
+                string elapsedTimeTotal = String.Format("{0:00}:{1:00}:{2:00}:{3:00}", tsTotal.Hours, tsTotal.Minutes, tsTotal.Seconds, tsTotal.Milliseconds/ 10);
+                Singleton.Instance.ShowElapsedTime(elapsedTimeTotal);
+                repository.Insert(sizex, elapsedTimeTotal);
+            }
+
+            // Repetição para diferentes tamanhos e estratégias
+            foreach (int sizex in sizes)
+            {
+                Stopwatch stopWatch0 = new Stopwatch();
+                int[] array = generateArray.CreateArray(sizex);
+                stopWatch0.Start();
+                var context = new Context(new SortClient(new CountingSort()));
+                context.ExecuteStrategy(array);
+                stopWatch0.Stop();
+                TimeSpan ts = stopWatch0.Elapsed;
+                string elapsedTime = String.Format("{1:00}:{2:00}:{3:00}", ts.Hours, ts.Minutes, ts.Seconds, ts.Milliseconds);
+                Singleton.Instance.ShowElapsedTime(elapsedTime);
+                repository.InsertData2("Counting Sort", sizex, elapsedTime);
+
+            }
+
+            foreach (int sizex in sizes)
+            {
+                Stopwatch stopWatch1 = new Stopwatch();
+                int[] array = generateArray.CreateArray(sizex);
+                stopWatch1.Start();
+                var context = new Context(new SortClient(new InsertionSort()));
+                context.ExecuteStrategy(array);
+                stopWatch1.Stop();
+                TimeSpan ts = stopWatch1.Elapsed;
+                string elapsedTime = String.Format("{1:00}:{2:00}:{3:00}", ts.Hours, ts.Minutes, ts.Seconds, ts.Milliseconds);
+                Singleton.Instance.ShowElapsedTime(elapsedTime);
+                repository.InsertData2("Insertion Sort", sizex, elapsedTime);
+
+            }
+
+            foreach (int sizex in sizes)
+            {
+                Stopwatch stopWatch2 = new Stopwatch();
+                int[] array = generateArray.CreateArray(sizex);
+                stopWatch2.Start();
+                var context = new Context(new SortClient(new QuickSort()));
+                context.ExecuteStrategy(array);
+                stopWatch2.Stop();
+                TimeSpan ts = stopWatch2.Elapsed;
+                string elapsedTime = String.Format("{1:00}:{2:00}:{3:00}", ts.Hours, ts.Minutes, ts.Seconds, ts.Milliseconds);
+                Singleton.Instance.ShowElapsedTime(elapsedTime);
+                repository.InsertData2("Quick Sort", sizex, elapsedTime);
+
+            }
+
+            foreach (int sizex in sizes)
+            {
+                Stopwatch stopWatch3 = new Stopwatch();
+                int[] array = generateArray.CreateArray(sizex);
+                stopWatch3.Start();
+                var context = new Context(new SortClient(new MargeSort()));
+                context.ExecuteStrategy(array);
+                stopWatch3.Stop();
+                TimeSpan ts = stopWatch3.Elapsed;
+                string elapsedTime = String.Format("{1:00}:{2:00}:{3:00}", ts.Hours, ts.Minutes, ts.Seconds, ts.Milliseconds);
+                Singleton.Instance.ShowElapsedTime(elapsedTime);
+                repository.InsertData2("Marge Sort", sizex, elapsedTime);
+
+            }
+            
+            Console.ReadKey();
+        }
+    }
+}
